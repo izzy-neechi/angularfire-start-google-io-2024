128d2d514b4db03f1ea357015a2836f73702f7fa
Israel Guzman <i.guzman@ubroapps.com>
2024-06-10 21:33:43 -0600

    codelab starting point

---
+++ src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
+    provideRouter(routes),
+  ],
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyA9PVhbIIqsZ5m0bp3YrmkYkYxYVpMXpVQ',
+    authDomain: 'codelab-io-2024.firebaseapp.com',
+    projectId: 'codelab-io-2024',
+    storageBucket: 'codelab-io-2024.appspot.com',
+    messagingSenderId: '822194742460',
+    appId: '1:822194742460:web:cd700d36dbe3b3268ddcb9',
+    measurementId: 'G-WKV0RTM3YK',
+  },
+};
